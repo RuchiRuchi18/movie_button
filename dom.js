0d2c62ef1354bae04cd55e7deeb9947b3eb54951
RuchiRuchi18 <101448424+RuchiRuchi18@users.noreply.github.com>
2023-07-20 15:13:25 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,16 @@
+let RED=document.getElementById("red");
+RED.onclick=function(){
+	box.style.backgroundColor="red";
+	}
+let BLUE=document.getElementById("blue");
+BLUE.onclick=function(){
+	box.style.backgroundColor="blue";
+	}
+let GREEN=document.getElementById("green");
+GREEN.onclick=function(){
+	box.style.backgroundColor="green";
+	}
+let YELLOW=document.getElementById("yellow");
+YELLOW.onclick=function(){
+	box.style.backgroundColor="yellow";
+	}
